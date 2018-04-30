4d2d3d7696953a17d51747b7784caf1841e565d4
gregwhitaker <gwhitake@gmail.com>
2018-04-29 18:18:14 -0700

    Doing work

--- src/main/java/ratpack/multiserver/example/service/hello/DefaultHelloMessageService.java
+++ src/main/java/ratpack/multiserver/example/service/hello/DefaultHelloMessageService.java
@@ -1,8 +1,18 @@
 package ratpack.multiserver.example.service.hello;
 
 import com.google.inject.Singleton;
+import ratpack.exec.Promise;
 
 @Singleton
 public class DefaultHelloMessageService implements HelloMessageService {
 
+    @Override
+    public Promise<String> getMessage() {
+        return null;
+    }
+
+    @Override
+    public Promise<Long> getMessageCount() {
+        return null;
+    }
 }
--- src/main/java/ratpack/multiserver/example/service/hello/HelloMessageService.java
+++ src/main/java/ratpack/multiserver/example/service/hello/HelloMessageService.java
@@ -1,4 +1,10 @@
 package ratpack.multiserver.example.service.hello;
 
+import ratpack.exec.Promise;
+
 public interface HelloMessageService {
+
+    Promise<String> getMessage();
+
+    Promise<Long> getMessageCount();
 }
