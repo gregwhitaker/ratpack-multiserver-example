aceefce26a8258dd2e7449b355b7d74e0c6b1b08
gregwhitaker <gwhitake@gmail.com>
2018-04-29 18:20:34 -0700

    Doing work

--- src/main/java/ratpack/multiserver/example/service/hello/DefaultHelloMessageService.java
+++ src/main/java/ratpack/multiserver/example/service/hello/DefaultHelloMessageService.java
@@ -1,18 +1,26 @@
 package ratpack.multiserver.example.service.hello;
 
 import com.google.inject.Singleton;
+import ratpack.exec.Blocking;
 import ratpack.exec.Promise;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Singleton
 public class DefaultHelloMessageService implements HelloMessageService {
 
+    private final AtomicLong count = new AtomicLong(0);
+
     @Override
     public Promise<String> getMessage() {
-        return null;
+        return Blocking.get(() -> {
+            count.incrementAndGet();
+            return "Hello, World!";
+        });
     }
 
     @Override
     public Promise<Long> getMessageCount() {
-        return null;
+        return Blocking.get(count::get);
     }
 }
